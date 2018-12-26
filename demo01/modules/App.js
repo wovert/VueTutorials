1bf77dcd25c619e8cff150c0d37bf85ed74156fc
wovert <67668283@qq.com>
2018-12-26 14:02:13 +0800

    webpack build

---
+++ demo01/modules/App.js
@@ -0,0 +1,12 @@
+var app = {
+  template: `<div>
+    我是App组件
+  </div>`
+}
+
+export var num1 = 1
+var num2 = 2
+
+export {num2}
+
+export default app
---
+++ demo01/modules/main.js
@@ -0,0 +1,14 @@
+// entry
+import Vue from '../bower_components/vue/dist/vue'
+import App from './App'
+import {num1, num2} from './App'
+
+console.log(num1, num2)
+
+new Vue({
+  el: '#app',
+  components: {
+    app: App
+  },
+  template: `<app/>`
+})
