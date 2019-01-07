4057c5207cfd4a337d1e4808fd270cacc47a551b
wovert <67668283@qq.com>
2019-01-07 15:20:50 +0800

    create menu components

---
+++ demo-cms/src/components/Member/Member.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    我是会员
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ demo-cms/src/components/Search/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    我是查找
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
